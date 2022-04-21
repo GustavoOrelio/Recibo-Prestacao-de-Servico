6f20cb3a314cd10c7e6dfd65c13c30e84cafd87f
Gustavo Orelio <oreliogustavo@gmail.com>
2022-04-21 20:37:11 -0300

    Iniciando com JavaScrcit

---
+++ script.js
@@ -0,0 +1,16 @@
+<>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.mask.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.valor').mask( "#.##0,00", {reversed: true} )
+    })
+</script>
+</>
+
+
+
